481436dcf6ec2b1c8412855e0c144eed515e691c
Jason <grimlock773@gmail.com>
2017-10-31 05:45:25 -0400

    Started Basic Modular Coding

--- Program.cs
+++ Program.cs
@@ -10,9 +10,30 @@ namespace ConsoleApplication8
     {
         static void Main(string[] args)
         {
-          console.WriteLine();
-            Console.WriteLine();
+            Program Func = new Program();
+            Func.startMenu();
+            Console.ReadLine();
+
         }
+        public void startMenu()
+        {
+            Program Func = new Program();
+            Console.Clear();
+            centText("\n\n\n\n\n");
+            centText("Welcome to Frequency");
+            centText("'Prepare to be beat'");
+            centText("Type Credits for Information");
+            centText("-------------------------");
+            centText("Press Enter to Start Game");
+            Console.ReadLine();
+        }
+        static void centText(string arg)
+        {
+            Program Func = new Program();
+            int xOffset = Console.WindowWidth / 2 + arg.Length / 2;
+            Console.WriteLine(arg.PadLeft(xOffset));
+        }
+
     }
 }
-b
+
